9611a9bbda745ea79e82c192099bfca72b458e8c
riqua-end <riqua1@naver.com>
2023-06-05 17:31:59 +0900

    BoardMapperTest xml방식과 어노테이션방식

---
+++ ex02/src/main/java/org/ezen/ex02/mapper/BoardMapper.java
@@ -0,0 +1,16 @@
+package org.ezen.ex02.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.ezen.ex02.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	//mybatis의 @select 어노테이션으로 처리
+	//SQL문을 작성할때 ;이 없어야 한다
+	//@Select("select * from tbl_board where bno > 0") 주석 달면 xml을 이용하여 처리
+	//mybatis는 select시 ResultSet으로 반환되는것을 자바 컬렉션으로 반환
+	public List<BoardVO> getList();
+	
+}
---
+++ ex02/src/test/java/org/ezen/ex02/mapper/BoardMapperTests.java
@@ -0,0 +1,32 @@
+package org.ezen.ex02.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+
+//mybatis이용한 tbl_board관련 테스트
+public class BoardMapperTests {
+	
+	//Setter를 이용한 주입 (BoardMapper를 주입 받음)
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	//BoardMapper인터페이스의 getList()메서드 테스트
+	public void testGetList() {
+		
+		mapper.getList().forEach(board -> log.info(board));
+		//mapper는 인터페이스의 참조 변수로 인터페이스를 구현을 안함 (즉 스프링이 자동으로 해줌)
+		//List<BoardVO>로 반환
+	}
+	
+}
