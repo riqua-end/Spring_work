7b89e271430056fc9e5c7e5df46114d075c629e3
riqua-end <riqua1@naver.com>
2023-06-05 17:33:23 +0900

    BoardMapper 인터페이스 @Select 어노테이션으로 처리

--- ex02/src/main/java/org/ezen/ex02/mapper/BoardMapper.java
+++ ex02/src/main/java/org/ezen/ex02/mapper/BoardMapper.java
@@ -9,7 +9,8 @@ public interface BoardMapper {
 	
 	//mybatis의 @select 어노테이션으로 처리
 	//SQL문을 작성할때 ;이 없어야 한다
-	//@Select("select * from tbl_board where bno > 0") 주석 달면 xml을 이용하여 처리
+	@Select("select * from tbl_board where bno > 0")
+	//주석 달면 xml을 이용하여 처리
 	//mybatis는 select시 ResultSet으로 반환되는것을 자바 컬렉션으로 반환
 	public List<BoardVO> getList();
 	
