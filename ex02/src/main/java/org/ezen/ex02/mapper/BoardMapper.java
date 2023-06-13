c011b66efab754f96dc30e5653c99e4f3cf6f8b9
riqua-end <riqua1@naver.com>
2023-06-13 14:43:23 +0900

    myBatis 게시글 페이징 처리 Mapper,mapper.xml

--- ex02/src/main/java/org/ezen/ex02/mapper/BoardMapper.java
+++ ex02/src/main/java/org/ezen/ex02/mapper/BoardMapper.java
@@ -2,8 +2,8 @@ package org.ezen.ex02.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.ezen.ex02.domain.BoardVO;
+import org.ezen.ex02.domain.Criteria;
 
 public interface BoardMapper {
 	
@@ -13,6 +13,9 @@ public interface BoardMapper {
 	//주석 달면 xml을 이용하여 처리
 	//mybatis는 select시 ResultSet으로 반환되는것을 자바 컬렉션으로 반환
 	public List<BoardVO> getList();
+	
+	//페이지관련 Criteria객체를 파라메터로 갖는 메서드
+	public List<BoardVO> getListWithPaging(Criteria cri);
 	
 	//PK값인 tbl_board테이블의 bno에 들어가는 seq_board의 nextval값을 미리 알필요 없는 경우
 	public void insert(BoardVO board);
