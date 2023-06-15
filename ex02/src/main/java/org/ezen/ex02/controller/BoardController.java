5a7ca43741a16e8ffcb04058b46764ccf78f6da0
riqua-end <riqua1@naver.com>
2023-06-15 16:33:51 +0900

    UriComponentsBuilder를 이용하는 링크 생성

--- ex02/src/main/java/org/ezen/ex02/controller/BoardController.java
+++ ex02/src/main/java/org/ezen/ex02/controller/BoardController.java
@@ -129,14 +129,15 @@ public class BoardController {
 			//return redirect:하는 페이지로 속성값 전달
 			rttr.addFlashAttribute("result", "success");
 		}
+		/*
 		rttr.addAttribute("pageNum",cri.getPageNum());
 		rttr.addAttribute("amount",cri.getAmount());
 		//list로 검색조건을 넘김 rttr.addAttribute("type",cri.getType());
 		//검색처리 추가
 		rttr.addAttribute("type",cri.getType());
 		rttr.addAttribute("keyword",cri.getKeyword());
-		
-		return "redirect:/board/list";
+		*/
+		return "redirect:/board/list" + cri.getListLink();
 	}
 	
 	//삭제처리 --페이지 미처리
@@ -158,11 +159,13 @@ public class BoardController {
 		if (service.remove(bno)) {
 			rttr.addFlashAttribute("result", "success");
 		}
+		/*
 		rttr.addAttribute("pageNum",cri.getPageNum());
 		rttr.addAttribute("amount",cri.getAmount()); //list로 검색 조건을 넘김
 		//검색처리 추가
 		rttr.addAttribute("type",cri.getType());
 		rttr.addAttribute("keyword",cri.getKeyword());
-		return "redirect:/board/list";
+		*/
+		return "redirect:/board/list" + cri.getListLink();
 	}
 }
