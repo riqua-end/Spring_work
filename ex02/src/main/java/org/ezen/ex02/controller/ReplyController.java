6714acdab77083fca79d49f06b910829cb37533f
riqua-end <riqua1@naver.com>
2023-06-19 17:38:42 +0900

    댓글 삭제 수정 조회

--- ex02/src/main/java/org/ezen/ex02/controller/ReplyController.java
+++ ex02/src/main/java/org/ezen/ex02/controller/ReplyController.java
@@ -8,11 +8,13 @@ import org.ezen.ex02.service.ReplyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -60,5 +62,38 @@ public class ReplyController {
 		
 		return new ResponseEntity<>(service.getList(cri, bno),HttpStatus.OK);
 		//public List<ReplyVO> getList(Criteria cri, Long bno)
+	}
+	
+	@GetMapping(value = "/{rno}",produces = {MediaType.APPLICATION_JSON_VALUE})
+	//지정 댓글 정보 조회
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		
+		log.info("get : " + rno);
+		
+		return new ResponseEntity<>(service.get(rno),HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		
+		log.info("remove : " + rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<>("success" , HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method = {RequestMethod.PUT,RequestMethod.PATCH},value = "/{rno}",
+			consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE+ ";charset=UTF-8"})
+	//rno값과 json으로 된 ReplyVO멤버변수 값이 옴
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+		
+		vo.setRno(rno);
+		
+		log.info("rno : " + rno);
+		log.info("modify : " + vo);
+		
+		return service.modify(vo) == 1 ? new ResponseEntity<> ("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
