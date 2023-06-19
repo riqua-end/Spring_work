c856872fe672743f4c3e5299f6ac68ac7e014c40
riqua-end <riqua1@naver.com>
2023-06-19 14:40:07 +0900

    댓글 처리 Create

--- ex02/src/main/java/org/ezen/ex02/mapper/ReplyMapper.java
+++ ex02/src/main/java/org/ezen/ex02/mapper/ReplyMapper.java
@@ -1,5 +1,9 @@
 package org.ezen.ex02.mapper;
 
-public interface ReplyMapper {
+import org.ezen.ex02.domain.ReplyVO;
 
+public interface ReplyMapper {
+	//댓글 작성 (CRUD중 Create)
+	public int insert(ReplyVO vo);
+	
 }
--- ex02/src/test/java/org/ezen/ex02/mapper/ReplyMapperTests.java
+++ ex02/src/test/java/org/ezen/ex02/mapper/ReplyMapperTests.java
@@ -1,5 +1,8 @@
 package org.ezen.ex02.mapper;
 
+import java.util.stream.IntStream;
+
+import org.ezen.ex02.domain.ReplyVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +20,31 @@ public class ReplyMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
+	private Long[] bnoArr = { 56L,57L,58L,59L,60L }; //게시글 번호 배열
+	/*
 	@Test
 	//mapper객체 주입 여부 확인
 	public void testMapper() {
 		
 		log.info("mapper-----------" + mapper);
+	}
+	*/
+	
+	@Test
+	public void testCreate() {
+		
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			//rangeClosed(1,10)은 1부터 10까지 1씩 증가하면서 선택
+			//forEach는 1부터 10까지 반복
+			ReplyVO vo = new ReplyVO();
+			
+			//게시물의 번호
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReplyer("replyer " + i);
+			
+			mapper.insert(vo);
+		});
 	}
 	
 }
